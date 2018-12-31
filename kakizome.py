e8db7ebfb2b1bf0434053635b9e614afde1df099
Koki Tanaka <h27i18@ed.cc.suzuka-ct.ac.jp>
2019-01-01 04:37:06 +0900

    goji

--- kakizome.py
+++ kakizome.py
@@ -5,45 +5,35 @@ print('''
 
 
 
+                        			                      JMMMMM#                                         
+	                                       		             (MMMF                  .
+                  			                            (MMMF                .JMMa.                  
+  			                     	                   (MMMF               .MMMMMMm,            
+        			                MMMMH""""""""""""""WMMMM#""""""""""""""""""""""""""^                  
+                                			          .MMMMD                                          
+                 			                        .MMM#!
+                			              +,.       .MMM=          .MNJ,                                    
+                        			       7MNg,.  .MM"           .MMMMMMm
+             	          			          (WMMN+MD           .MMMMM=                               
+                                 			  ,WMMMNJ.       .(MMMM"           
+                     			        	   (MMMMN.     .MMMM"                                         
+                                  			     TMMM   ..MMMMD       .N,.       
+                         			               ,W$ .(MMMMD        .MMMMNa,                       
+ 			                                         .dMMMM"        .MMMMMB"""          
+                        			               ..MMMM9!        .dMMMM@!                              
+                   				             ..MMMM"'         .dMMMM@                                    
+                			                 ..MMMM"`          .MMMMMD                  
+        			                      ..MMM9^           ..MMMMM=                                    
+       				                   ..MM#"^             .dMMMMD?TMNJ.                        
+        			                ,9"!               .gMMMM@^     ?MMMN&.           
+                			                       ..MMMMM9^          ,WMMMMN,
+                			                    .+MMMM#"                .TMMMMMN,                        
+                			               ..+MMMM""`                     .WMMMMM[                  
+              				            ..(MMMY"=`                            ,MMMM]    
+              				         .M"""!                                     7MM'             
 
-
-                                                                 `  .JQQo.         .-.               `(QQQQQmmx
-                                                                 .(dMMMMNR-      .+dNHs-.`            jMMMMMMMI
-                                                                 (WMMMMMMNmx.   (dMMMMMNA..         ` jMMMMMMMI
-                                                                  ?WMMMMMMMNs-.jdMMMMMMMM$_  `  `    `jMMMMMMMI     `   `   ` .J+-.
-                                                                   (WMMMMMMMNNNMMMMMMMMHC~(((((((((((+dMMMMMMMR+(((((((((-. .(dNNNmo-.
-                                                                    _dMMMMMMMMMMMMMMMM9! (MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMHz(jMMMMMMMNk<
-                                                                     (vMMMMMMMMMMMMM8:  `(MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNQWMMMMMMMM#C!
-                                                                     .(WMMMMMMMMMM9C`   `(MNMMNMMMMNMMMMMMMMMMMNMMNMMNMMMMMMMMMMMMM9!
-                                                               `   (igNMMMMMMMMMMD!       ????????????UMMMMMMM87??????7WMMMMMMMMMBC`
-                                                                .JdNMMMMMMMMMMMMNk_                   jMMMMMMMI     .jdMMMMMMMMM$!
-                                                               jWMMMMMMMMMMMMMMMM#_                   jMMMMMMMI   .JdMMMMMMMMM9!`
-                                                               ?XMMMMMMMMMMMMMMMMNl- ..(JJJJJJJJJJJJJJdMMMMMMMmJJJgMMMMMMMMMNmeJJJJJ..
-                                                                -?WMMMMY>_~dMMMMMNk< +XMMMMMMMMNMMNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN>_
-                                                                  ?7UC!`   (MMMMMMHI jXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN>_
-                                                                           (XMMMMMMR-+UMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMB>_
-                                                                           (dMMMMMMK<__<<<<<<<<<<<<<<<1jXMMMMMMMMMMMBC<<<<<<<<<<<<<<!
-                                                                         .(dMMMMMMMH>_              .JgHMMMMMMMMMM9C`
-                                                                        (gMMMMMMMMMNI_           .JQNMMMMMMMMMMMY>_
-                                                                      .dMMMMMMMMMMMNI_       ..JdHMMMMMMMMMMMMMMNNNNNNNNNNNNNNmy_
-                                                                 ` ..dHMMMMMMMMMMMMNk_   ..(uXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMR~
-                                                               ` .(dMMMMMMMMMMMMMMMMR+.J&gMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMR~
-                                                                (gMMMMMMMMMMUMMMMMMMHmHMMMMMMMMMMMMMMM#Y777777777777777TMMMMMMMR~
-                                                               _dMMMMMMMMMY!_dNMMMMMMUWMMMMMMMMMMMMMMMK~               (WMMMMMMR~
-                                                                _?MMMMMM9!`  dNMMMMNN>(WMMMMMMMMMMMMMMR-...............(WMMMMMMR~
-                                                                  ?ZMM8>`    dMMMMMMN{  ?HBY>`(dMMMMMMMNNHHHHHHHHHHHHHNMMMMMMMMR~
-                                                                    ?!`      dMMMMMM#:        (dHMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMR~
-                                                                            .dMMMMMMK:        (dHMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMR~
-                                                                            (dMMMMMMS!        (dHMMMMM#C<<<<<<<<<<<<<<?vMMMMMMMR~
-                                                                           .(WMMMMMN0~        (dHMMMMMK~               (WMMMMMMR~
-                                                                           (dHMMMMM#I_        (dHMMMMMK_  ` ` ` ` ` `  (WMMMMMMR~
-                                                                 ``      ..jWMMMMMMK:         (dHMMMMMNmggggggggggggggggMMMMMMMR~
-                                                                  (jQNNNNNNNMMMMMMM$`         (dHMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMR~
-                                                                   jMMMMMMMMMMMMMM3~          (dHMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMR~
-                                                                   (ZMMMMMMMMMMMY!            (dHMMMMMMMMHHHHHHHHHHHHHMMMMMMMMMR~
-                                                                    (dMMMMMH9Y=!`             (dHMMMMMK< ` ` ` ` ` ` ` (WMNMNMMS~
-                                                                     _???!!~`                  <?TTTTT>`                ???????!
-
+                                                       
+                                                                                             
 
 
 
