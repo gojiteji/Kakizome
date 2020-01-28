efbba8b3bf65a2c7a671b99e728d0e994db42e26
gojiteji <h27i18@ed.cc.suzuka-ct.ac.jp>
2020-01-28 14:49:34 +0900

    2020

--- kakizome.py
+++ kakizome.py
@@ -3,36 +3,42 @@
 print('''
 
 
-
-
-                        			                      JMMMMM#                                         
-	                                       		             (MMMF                  .
-                  			                            (MMMF                .JMMa.                  
-  			                     	                   (MMMF               .MMMMMMm,            
-        			                MMMMH""""""""""""""WMMMM#""""""""""""""""""""""""""^                  
-                                			          .MMMMD                                          
-                 			                        .MMM#!
-                			              +,.       .MMM=          .MNJ,                                    
-                        			       7MNg,.  .MM"           .MMMMMMm
-             	          			          (WMMN+MD           .MMMMM=                               
-                                 			  ,WMMMNJ.       .(MMMM"           
-                     			        	   (MMMMN.     .MMMM"                                         
-                                  			     TMMM   ..MMMMD       .N,.       
-                         			               ,W$ .(MMMMD        .MMMMNa,                       
- 			                                         .dMMMM"        .MMMMMB"""          
-                        			               ..MMMM9!        .dMMMM@!                              
-                   				             ..MMMM"'         .dMMMM@                                    
-                			                 ..MMMM"`          .MMMMMD                  
-        			                      ..MMM9^           ..MMMMM=                                    
-       				                   ..MM#"^             .dMMMMD?TMNJ.                        
-        			                ,9"!               .gMMMM@^     ?MMMN&.           
-                			                       ..MMMMM9^          ,WMMMMN,
-                			                    .+MMMM#"                .TMMMMMN,                        
-                			               ..+MMMM""`                     .WMMMMM[                  
-              				            ..(MMMY"=`                            ,MMMM]    
-              				         .M"""!                                     7MM'             
-
-                                                       
+                             ■      
+                            ■■■     
+    ■■■■■■■■■■■■■■■■■■■■■■■■■■■■    
+                          ■■■■■     
+                         ■■■■■      
+                        ■■■■        
+                       ■■■          
+                      ■■■           
+                     ■■             
+                   ■■               
+                 ■■■■               
+                 ■■■                
+                 ■■■                
+                 ■■■                
+                 ■■■           ■■■  
+                 ■■■           ■■■■ 
+■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+                 ■■■                
+                 ■■■                
+                 ■■■                
+                 ■■■                
+                 ■■■                
+                 ■■■                
+                 ■■■                
+                 ■■■                
+                 ■■■                
+                 ■■■                
+                 ■■■                
+                 ■■■                
+                 ■■■                
+                 ■■■                
+                 ■■■                
+          ■■    ■■■■                
+            ■■■■■■■                 
+              ■■■■■                 
+              ■■■                             
                                                                                              
 
 
